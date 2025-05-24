05d692d182dbe1add558bf78b4cae3d5999e90d6
ikhlashmulya <nurahmanmulya@gmail.com>
2025-05-24 14:06:55 +0700

    first commit

---
+++ app.py
@@ -0,0 +1,33 @@
+from flask import Flask, jsonify, request
+import joblib
+import os
+
+SECRET_TOKEN = os.environ.get('SECRET_TOKEN') or 'ansaju'
+MODEL_PATH = os.environ.get('MODEL_PATH') or 'model_ansaju.pkl'
+
+model = joblib.load(MODEL_PATH)
+
+app = Flask(__name__)
+
+def require_auth(f):
+    from functools import wraps
+    @wraps(f)
+    def wrapper(*args, **kwargs):
+        auth_header = request.headers.get("Authorization", "")
+        token = auth_header.replace("Bearer ", "")
+        if token != SECRET_TOKEN:
+            return jsonify({"error": "Unauthorized"}), 401
+        return f(*args, **kwargs)
+    return wrapper
+
+@app.route('/predict', methods=['POST'])
+@require_auth
+def predict():
+    data = request.get_json()
+    features = data['features']
+
+    prediction = model.predict([features])
+    return jsonify(prediction=prediction[0])
+
+if __name__ == '__main__':
+    app.run()
